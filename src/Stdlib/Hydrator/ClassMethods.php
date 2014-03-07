472f7e75eb2d9b5113290ce49cdf530fb6640f79
Дмитрий Пацура <zaets28rus@gmail.com>
2014-03-07 08:25:57 +0400

    Added simple Hydrator for ClassMethods in Stdlib

---
+++ src/Stdlib/Hydrator/ClassMethods.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author: Patsura Dmitry <zaets28rus@gmail.com>
+ * @date: 07.03.14 14:05
+ */
+
+namespace Phalcony\Stdlib\Hydrator;
+
+use BadMethodCallException;
+
+class ClassMethods {
+    static public function hydrate(array $data, $object)
+    {
+        if (!is_object($object)) {
+            throw new BadMethodCallException(sprintf(
+                '%s expects the provided $object to be a PHP object)', __METHOD__
+            ));
+        }
+
+        $transform = function ($letters) {
+            $letter = substr(array_shift($letters), 1, 1);
+            return ucfirst($letter);
+        };
+
+        foreach ($data as $property => $value) {
+            $method = 'set' . ucfirst($property);
+
+            if (method_exists($object, $method)) {
+
+                $object->$method($value);
+            }
+        }
+
+        return $object;
+    }
+}
