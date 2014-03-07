f6978feec5062ee53eaf9027a36cf24774a0fab7
Дмитрий Пацура <zaets28rus@gmail.com>
2014-03-07 08:26:22 +0400

    Rewrite Application class for easy init di services

--- src/Application.php
+++ src/Application.php
@@ -7,6 +7,7 @@
 namespace Phalcony;
 
 use Phalcon\Events\Manager as EventsManager;
+use Phalcony\Stdlib\Hydrator\ClassMethods;
 
 class Application
     extends \Phalcon\Mvc\Application
@@ -72,17 +73,15 @@ class Application
         $config = &$this->configuration;
 
         $loader = new \Phalcon\Loader();
-        $loadNamespaces = array();
+        $loadNamespaces = $config['application']['registerNamespaces'];
 
-        foreach ($config['modules'] as $module => $enabled) {
+        foreach ($config['application']['modules'] as $module => $enabled) {
             $moduleName = ucfirst($module);
             $loadNamespaces[$moduleName.'\Model'] = APPLICATION_PATH . '/modules/'.$module.'/models/';
             $loadNamespaces[$moduleName.'\Service'] = APPLICATION_PATH . '/modules/'.$module.'/services/';
         }
 
-        $loadNamespaces['Service'] = APPLICATION_PATH . '/services/';
-
-        $loader->registerDirs($config['registerDirs'])
+        $loader->registerDirs($config['application']['registerDirs'])
             ->registerNamespaces($loadNamespaces)
             ->register();
 
@@ -90,25 +89,45 @@ class Application
 
     /**
      * @return $this
+     * @throws \Exception
      */
     public function bootstrap()
     {
         $di = $this->getDI();
 
         $this->_registerLoader();
-        $this->registerModules($this->configuration['modules']);
+        $this->registerModules($this->configuration['application']['modules']);
 
         $eventsManager = new EventsManager();
         $this->setEventsManager($eventsManager);
 
-        $methods = $this->getInitServices();
-        foreach($methods as $name => $method) {
-            $returnValue = $this->{$method->getName()}();
-            $di->set($name, function() use($returnValue) {return $returnValue;});
-        }
+        if (isset($this->configuration['services'])) {
+            if (!is_array($this->configuration['services'])) {
+                throw new \Exception('Config[services] must be an array');
+            }
 
-        iconv_set_encoding('internal_encoding', 'UTF-8');
-        setlocale(LC_ALL, 'ru_RU.UTF-8');
+            if (count($this->configuration['services']) > 0) {
+                foreach($this->configuration['services'] as $diServiceName => $serviceParameters) {
+                    $class = $serviceParameters['class'];
+
+                    if (gettype($class) != 'object') {
+                        if (isset($serviceParameters['__construct'])) {
+                            $service = new $class($serviceParameters['__construct']);
+                        } else {
+                            $service = new $class();
+                        }
+                    } else {
+                        $service = $class;
+                    }
+
+                    if (isset($serviceParameters['parameters'])) {
+                        $service = ClassMethods::hydrate($serviceParameters['parameters'], $service);
+                    }
+
+                    $di->set($diServiceName, $service);
+                }
+            }
+        }
 
         return $this;
     }
@@ -119,25 +138,5 @@ class Application
     public function run()
     {
         $this->handle($_SERVER['REQUEST_URI'])->send();
-    }
-
-    /**
-     * Get class init methods
-     *
-     * @return array
-     */
-    protected function getInitServices()
-    {
-        $class        = new \ReflectionObject($this);
-        $classMethods = $class->getMethods();
-
-        $classResources = array();
-        foreach ($classMethods as $method) {
-            if (5 < strlen($method->getName()) && 'init' === substr($method->getName(), 0, 4)) {
-                $classResources[lcfirst(substr($method->getName(), 4))] = $method;
-            }
-        }
-
-        return $classResources;
     }
 }
