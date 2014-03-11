5c23d56801be709d8f4e6e230387a91f57caf36f
Дмитрий Пацура <zaets28rus@gmail.com>
2014-03-11 15:52:52 +0400

    Added ClassProperties hydrator

---
+++ src/Stdlib/Hydrator/ClassProperties.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author: Patsura Dmitry <zaets28rus@gmail.com>
+ * @date: 08.03.14 22:07
+ */
+
+namespace Phalcony\Stdlib\Hydrator;
+
+use BadMethodCallException;
+
+class ClassProperties
+{
+    public static function hydrate(array $data, $object)
+    {
+        if (!is_object($object)) {
+            throw new BadMethodCallException(sprintf(
+                '%s expects the provided $object to be a PHP object)',
+                __METHOD__
+            ));
+        }
+
+        foreach ($data as $property => $value) {
+            if (property_exists($object, $property)) {
+                $object->{$property} = $value;
+            }
+        }
+
+        return $object;
+    }
+}
