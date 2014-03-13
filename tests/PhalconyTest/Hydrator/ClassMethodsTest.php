5a28a911deb6b85bd8711a108b6e1f4cf635c349
Дмитрий Пацура <zaets28rus@gmail.com>
2014-03-13 15:43:45 +0400

    ClassMethodsTest add success test

--- tests/PhalconyTest/Hydrator/ClassMethodsTest.php
+++ tests/PhalconyTest/Hydrator/ClassMethodsTest.php
@@ -15,4 +15,26 @@ class ClassMethodsTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('BadMethodCallException', 'hydrate expects the provided $object to be a PHP object');
         ClassMethods::hydrate(array(), 1);
     }
+
+    public function testHydrateSuccess()
+    {
+        $object = new \Phalcony\Test\Object();
+
+        $this->assertEquals(null, $object->property1);
+        $this->assertEquals(null, $object->property2);
+        $this->assertEquals(null, $object->property3);
+
+        $object = ClassMethods::hydrate(
+            array(
+                'property1' => 1,
+                'property2' => 2,
+                'property3' => 3
+            ),
+            $object
+        );
+
+        $this->assertEquals(1, $object->property1);
+        $this->assertEquals(2, $object->property2);
+        $this->assertEquals(3, $object->property3);
+    }
 }
--- tests/PhalconyTest/Object.php
+++ tests/PhalconyTest/Object.php
@@ -6,8 +6,33 @@
 
 namespace Phalcony\Test;
 
-class Object {
+class Object
+{
     public $property1;
     public $property2;
     public $property3;
+
+    /**
+     * @param mixed $property1
+     */
+    public function setProperty1($property1)
+    {
+        $this->property1 = $property1;
+    }
+
+    /**
+     * @param mixed $property2
+     */
+    public function setProperty2($property2)
+    {
+        $this->property2 = $property2;
+    }
+
+    /**
+     * @param mixed $property3
+     */
+    public function setProperty3($property3)
+    {
+        $this->property3 = $property3;
+    }
 }
