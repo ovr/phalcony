95155e35292b7c9024d710c74cdf6cc62d187f13
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-03 20:06:33 +1000

    Add HeadScript

---
+++ src/HeadScript.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author: Patsura Dmitry <zaets28rus@gmail.com>
+ * @date: 07.03.14 15:19
+ */
+
+namespace Phalcony;
+
+/**
+ * Class HeadScript
+ * @package Phalcony
+ */
+class HeadScript
+{
+	protected $vars = array();
+
+	/**
+	 * @param $key
+	 * @param $value
+	 */
+	public function addVar($key, $value)
+	{
+		$this->vars[$key] = $value;
+	}
+
+	public function __toString()
+	{
+		if (count($this->vars) == 0) {
+			return '';
+		}
+
+		$html = '<script type="text/javascript">'."\n";
+
+		foreach($this->vars as $key => $value) {
+			$html .= "\t\tvar ".$key.' = '.json_encode($value).";\n";
+		}
+
+		return $html . "\t" . '</script>';
+	}
+} 
